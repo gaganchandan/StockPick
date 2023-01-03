6dfcc5dc83a771240a80f2d8ea59e659e693535f
Gagan Chandan <gaganchandan29@gmail.com>
2023-01-03 20:25:16 +0530

    Added test.py

--- backtests/test.py
+++ backtests/test.py
@@ -1,25 +1,32 @@
+from config import *
 import argparse
 import backtrader as bt
-from pathlib import Path
+import os
+import datetime
 
-global data
-data_folder = Path("../data")
-data_csv = data_folder / "nifty.csv"
-data = bt.feeds.GenericCSVData(dataname=data_csv,
+csv = os.path.join(root, "data", "nifty.csv")
+data = bt.feeds.GenericCSVData(dataname=csv,
                                dtformat=('%Y-%m-%d'),
                                datetime=0,
                                high=2,
                                low=3,
                                open=1,
                                close=4,
-                               volume=5),
+                               volume=5)
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--capital.', default=100000.0,
+parser.add_argument('--capital', default=100000.0,
                     help="Capital to start with")
 parser.add_argument('--strategy', default=None, help="Strategy to use")
+parser.add_argument('--name', default=datetime.datetime.now().strftime(
+    "%I:%M%p-%B-%d-%Y"), help="Name of output file")
+args = parser.parse_args()
+strategy = importlib.import_module(os.path.join(
+    root, strategies, (args.strategy+".Backtest")))
 
-cerebro = bt.Cerebro()  # Create a cerebro entity
-cerebro.broker.setcash(100000.0)  # Set the starting cash
+# Run the backtest using Cerebro
+cerebro = bt.Cerebro()  # Create a Cerebro entity
+cerebro.broker.setcash(args.capital)  # Set the starting cash
 cerebro.adddata(data)  # Add the data feed
-cerebro.addstrategy()  # Add the trading strategies
-cerebro.run()
+cerebro.addstrategy(strategy)  # Add the trading strategies
+cerebro.run()  # Run the  simulation
