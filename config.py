4094aa0985484f8736b271d4859f562aa2160f97
Gagan Chandan <gaganchandan29@gmail.com>
2023-01-04 18:51:19 +0530

    Added list of stocks and updated get.py.

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 import os
 root = os.path.dirname(os.path.abspath(__file__))
+symbols = ['NIFTYBEES', 'TATASTEEL', 'ZOMATO', 'COFFEEDAY', 'UNIONBANK']
--- data/get.py
+++ data/get.py
@@ -1,15 +1,18 @@
-from config import *
+import config
 from nsepy import get_history
 from datetime import date
 import os
 
-# Specify start and end date
+# Specify start and end date. 10 years worth of data will be used.
 start = date(2013, 1, 1)
 end = date(2022, 12, 31)
 
-# Get Nifty data
-data = get_history(symbol="NIFTY", start=start, end=end, index=True)
-# Convert all values of Open, Close, High and Low to float
-data = data.astype(float)
-# Save the data to a csv file
-data.to_csv(os.path.join(root, "data", "nifty.csv"))
+# Fetch historical data using nsepy.
+for symbol in config.symbols:
+    try:
+        # Check if the data is already present. If not, download it.
+        os.path.exists(os.path.join(config.root, "data", (symbol + ".csv")))
+    except:
+        data = get_history(symbol=symbol, start=start, end=end)
+        # Save the data to a csv file.
+        data.to_csv(os.path.join(config.root, "data", (symbol + ".csv")))
