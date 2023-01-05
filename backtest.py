78b95268a427c4e5bc86a7ea9776acd1fb9336b6
Gagan Chandan <gaganchandan29@gmail.com>
2023-01-05 11:29:04 +0530

    Fixed imports and writer errors.

--- backtest.py
+++ backtest.py
@@ -4,34 +4,35 @@ import backtrader as bt
 import os
 import datetime
 import importlib
-
+from strategies.SMACrossover import Backtest
 
 parser = argparse.ArgumentParser()
 
-# Take the value of capital to be used for the strategy
+# Take the value of capital to be used for the strategy.
 parser.add_argument('--capital', default=100000.0,
                     help="Capital to start with")
 
 # Take the name of the strategy from the user. The strategy should be in the
 # strategies folder in a file named {strategy}.py.
-parser.add_argument('--strategy', default=None, help="Strategy to use", choices=map(
-    (lambda string: string.strip(".py")), os.listdir(os.path.join(config.root, "strategies"))))
+parser.add_argument('--strategy', default=None, help="Strategy to use", choices=list(map(
+    (lambda string: string.strip(".py")), os.listdir(os.path.join(config.root, "strategies")))))
 
 # Take the symbols on which the backtest should be run. It should be specified
 # in the config file.
 parser.add_argument('--symbols', default=config.symbols,
                     nargs='+', choices=config.symbols, help="Symbols to use")
 
-# Take the name of the file in which to store backtest results
+# Take the name of the file in which to store backtest results.
 parser.add_argument('--name', default=datetime.datetime.now().strftime(
     "%I:%M%p-%B-%d-%Y"), help="Name of output file")
 
 args = parser.parse_args()
 
 # Import the strategy. The strategy file should contain a class called Backtest
-# which inherits from bt.Strategy
-strategy = importlib.import_module(os.path.join(
-    config.root, "strategies", (args.strategy+".Backtest")))
+# which inherits from bt.Strategy.
+strategy_file = importlib.import_module(
+    "strategies" + '.' + args.strategy)
+strategy = getattr(strategy_file, "Backtest")
 
 
 for symbol in args.symbols:
@@ -47,17 +48,20 @@ for symbol in args.symbols:
     data = bt.feeds.GenericCSVData(dataname=csv,
                                    dtformat=('%Y-%m-%d'),
                                    datetime=0,
-                                   high=2,
-                                   low=3,
-                                   open=1,
-                                   close=4,
-                                   volume=5)
+                                   high=5,
+                                   low=6,
+                                   open=4,
+                                   close=8,
+                                   volume=10)
 
-    # Run the backtest using Cerebro
-    cerebro = bt.Cerebro()  # Create a Cerebro entity
-    cerebro.broker.setcash(args.capital)  # Set the starting cash
-    cerebro.adddata(data)  # Add the data feed
-    cerebro.addstrategy(strategy)  # Add the trading strategies
+    # Run the backtest using Cerebro.
+    cerebro = bt.Cerebro()  # Create a Cerebro entity.
+    cerebro.broker.setcash(args.capital)  # Set the starting cash.
+    cerebro.adddata(data)  # Add the data feed.
+    cerebro.addstrategy(strategy)  # Add the trading strategies.
+
+    # Specify file to store the results of the backtest.
     cerebro.addwriter(bt.WriterFile, csv=True, out=os.path.join(
-        config.root, "results", args.name+".csv"))
-    cerebro.run()  # Run the  simulation
+        config.root, "backtests", args.name+".csv"))
+
+    cerebro.run()  # Run the  simulation.
