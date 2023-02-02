45b6d27fb62f059cf64c44751e092f3592f85cb9
Daiwik R <daiwik.bhasker04@gmail.com>
2023-02-02 14:07:43 +0530

    changed layout
    adadd

--- stockpick/stockpick.py
+++ stockpick/stockpick.py
@@ -4,19 +4,13 @@ from PIL import ImageTk, Image
 import os
 import datetime
 import subprocess
-
-
 # Run get.py to ensure that the latest data is available
 os.system("python3 " + os.path.join(config.root, "data", "get.py"))
-
-
 # Generate calls using scan.py
 process = subprocess.Popen(
     ["python3", os.path.join(config.root, "stockpick", "scan.py")])
 (calls, err) = process.communicate()
 exit_code = process.wait()
-
-
 # Function to plot price chart
 def plot_price_chart(symbol):
     end = datetime.date(2023, 2, 1)
@@ -29,26 +23,23 @@ def plot_price_chart(symbol):
     data = data.loc[start:end]
     data.plot(figsize=(16, 9))
     plt.show()
-
-
 # GUI
 window = Tk()
 window.title("STONKS PICKER")
-window.config(padx=25, pady=25, bg="#7a3530")
+window.config(padx=10, pady=10, bg="#7a3530")
 canvas = Canvas(width=1080, height=676, bg="#7a3530", highlightthickness=0)
 img = ImageTk.PhotoImage(Image.open(os.path.join(
     config.root, "images", "stonks_image.jpeg")))
 canvas.create_image(540, 338, image=img)
 # canvas.create_text(540,338,text="STOCKS PICKER",font=("Arial",30,"bold"),fill="white")
 # title_label=Label(text="STOCK PICKER",font=("Arial",30,"bold"),bg="#7a3530",fg="white")
-stock_compute = Button(text="COMPUTE", font=(
-    "Arial", 20, "bold"), bg="#7a3530", fg="white")
-Button2 = Button(text="Button2", font=(
-    "Arial", 20, "bold"), bg="#7a3530", fg="white")
+stock_compute = Button(text="COMPUTE", font=("Arial", 15, "bold"), bg="#7a3530", fg="white")
+#Button2 = Button(text="Butto", font=("Arial", 20, "bold"), bg="#7a3530", fg="white")
 Stock_entry=Entry(width=20)
-canvas.grid(row=0,column=0,columnspan=3)
-stock_compute.grid(row=1,column=0)
-Button2.grid(row=1,column=2)
-Stock_entry.grid(row=1,column=1)
+canvas.grid(row=0,column=0,columnspan=6)
+
+#Button2.grid(row=1,column=4)
+Stock_entry.grid(row=1,column=3) 
+stock_compute.grid(row=2,column=3)
 # title_label.grid(row=1,column=0)
 window.mainloop()
